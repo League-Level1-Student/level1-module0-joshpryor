65a4da37a8395f89f0d8b61fd65252f05362809f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-12 18:27:24 -0700

    sound fx   ip does not work

--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -1,20 +1,64 @@
 import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 import javax.swing.JApplet;
+import javax.swing.JButton;
 
-public class SoundEffects {
+import java.io.IOException;
+public  class SoundEffects implements ActionListener {
+	JFrame window = new JFrame();
+	JPanel panal = new JPanel();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
 
 	public static void main(String[] args) {
-		SoundEffects se = new SoundEffects();
-		se.playSound("sawing-wood-daniel_simon.wav");
+
+		
+		new SoundEffects().getGoing();
+		
 	}
-	
+
+	private void getGoing() {
+		window.add(panal);
+		panal.add(button1);
+		panal.add(button2);
+		button1.setText("sawing wood");
+		button1.addActionListener((ActionListener) this);
+		button2.setText("gunshot");
+		button2.addActionListener((ActionListener) this);
+		window.pack();
+		
+	}
 	
 	
 	
 	private void playSound(String fileName) {
 	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
 	     sound.play();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) arg0.getSource();
+		SoundEffects sawingWood = new SoundEffects();
+		if (buttonPressed == button1) {
+			sawingWood.playSound("sawing-wood-daniel_simon.wav");
+		}
+		SoundEffects gunshot = new SoundEffects();
+		if (buttonPressed == button2) {
+			
+		}
+		gunshot.playSound("gunshot1.wav");	
 	}
 
 }
