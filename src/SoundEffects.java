ffa5e05be0ae4ca8077f334312a7828135a8c9b0
league <league@wts14.attlocal.net>
2018-04-24 19:03:39 -0700

    sound effects done josh is awesome

--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -1,64 +1,72 @@
 import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 
 import javax.swing.JApplet;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import java.io.IOException;
-public  class SoundEffects implements ActionListener {
+public class SoundEffects implements ActionListener {
 	JFrame window = new JFrame();
 	JPanel panal = new JPanel();
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	JButton button4 = new JButton();
 
 	public static void main(String[] args) {
 
-		
 		new SoundEffects().getGoing();
-		
+
 	}
 
 	private void getGoing() {
 		window.add(panal);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 		panal.add(button1);
 		panal.add(button2);
+		panal.add(button3);
+		panal.add(button4);
 		button1.setText("sawing wood");
 		button1.addActionListener((ActionListener) this);
-		button2.setText("gunshot");
+		button2.setText("cymbal");
 		button2.addActionListener((ActionListener) this);
+		button3.setText("bass");
+		button3.addActionListener((ActionListener) this);
+		button4.setText("drum");
+		button4.addActionListener((ActionListener) this);
 		window.pack();
-		
+
 	}
-	
-	
-	
+
 	private void playSound(String fileName) {
-	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
-	     sound.play();
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		JButton buttonPressed = (JButton) arg0.getSource();
-		SoundEffects sawingWood = new SoundEffects();
+		// SoundEffects sawingWood = new SoundEffects();
 		if (buttonPressed == button1) {
-			sawingWood.playSound("sawing-wood-daniel_simon.wav");
+			playSound("sawing-wood-daniel_simon.wav");
 		}
-		SoundEffects gunshot = new SoundEffects();
-		if (buttonPressed == button2) {
-			
+		// SoundEffects gunshot = new SoundEffects();
+		else if (buttonPressed == button2) {
+			playSound("cymbal.wav");
 		}
-		gunshot.playSound("gunshot1.wav");	
+		// SoundEffects gunshot = new SoundEffects();
+		else if (buttonPressed == button3) {
+			playSound("bass.wav");
+		}
+		// SoundEffects gunshot = new SoundEffects();
+		else if (buttonPressed == button4) {
+			playSound("drum.wav");
+		}
+
 	}
 
 }
